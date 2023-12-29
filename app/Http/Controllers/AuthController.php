008f3176687d54014ccb887017a734f9ebfabeee
Nicolas <nicolas.aguilar180193@gmail.com>
2023-12-29 12:12:34 -0300

    Login feature

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UserLoginRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Services\TokenService;
+
+class AuthController extends Controller
+{
+    protected $tokenService;
+
+	public function __construct(TokenService $tokenService )
+    {
+        $this->tokenService = $tokenService;
+    }
+
+    public function login(UserLoginRequest $request)
+	{
+		$credentials = $request->only(['email', 'password']);
+		if (Auth::attempt($credentials)) {
+			$user = Auth::user();
+			$token = $this->tokenService->generarToken($user);
+
+			return response()->json([
+				'data' => [
+					'token' => $token,
+				],
+			]);
+		}
+		return response()->json(['errors' => ['message' => 'Invalid credentials']], 401);
+	}
+}

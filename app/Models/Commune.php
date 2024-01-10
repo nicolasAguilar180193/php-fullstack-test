f70a02ed856874397b8bda84d0b5928a2837c9b2
Nicolas <nicolas.aguilar180193@gmail.com>
2024-01-10 16:14:24 -0300

    Model Commune mutator in status attribute

--- app/Models/Commune.php
+++ app/Models/Commune.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Commune extends Model
@@ -16,5 +17,12 @@ class Commune extends Model
     
     public function region() {
         return $this->belongsTo(Region::class, 'id_reg', 'id_reg');
+    }
+
+    public function status(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => statusEnumToString($value),
+        );
     }
 }
